29a19b7e5c036eb9e5828ea624bec974b9d0d680
Mahdi Ghasemi <ghasemim875@gmail.com>
2023-11-17 22:55:16 +0330

    chore: schedule fetch-article cmd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Enums\ArticleDataSource;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -9,6 +10,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
+        $this->sheduleFetchArticlesCmd($schedule);
     }
 
     protected function commands(): void
@@ -16,5 +18,12 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+    }
+
+    private function sheduleFetchArticlesCmd(Schedule $schedule): void
+    {
+        $schedule->command('app:fetch-articles', [ArticleDataSource::GUARDIAN->value])->dailyAt('00:00');
+        $schedule->command('app:fetch-articles', [ArticleDataSource::NEWS_API->value])->dailyAt('01:00');
+        $schedule->command('app:fetch-articles', [ArticleDataSource::NEW_YORK_TIMES->value])->dailyAt('02:00');
     }
 }
